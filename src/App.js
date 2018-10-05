b48383be64cc1cd484d00329be2231a1fc8a1587
Damian Ubowski <damian.ubowski@softprime.pl>
2018-10-05 14:07:41 +0200

    Add simple bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Button} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button bsStyle="primary">Primary</Button>
+      <div className="container bg-primary">
+        <p className="text-primary">.text-primary</p>
+        <p className="text-secondary">.text-secondary</p>
+        <p className="text-success">.text-success</p>
+        <p className="text-danger">.text-danger</p>
       </div>
     );
   }
