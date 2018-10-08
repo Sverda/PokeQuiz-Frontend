a8f9466e6f0e77d3c07bd7d9121ce2aad583c1c0
Damian Ubowski <damian.ubowski@softprime.pl>
2018-10-08 11:07:17 +0200

    NavPanel: Add buttons

--- src/NavPanel.js
+++ src/NavPanel.js
@@ -7,7 +7,12 @@ const stylee = {
     height: "300px"
   },
 
-  row: {
+  cell: {
+    width: 100,
+    height: 100
+  },
+
+  button: {
     width: 100,
     height: 100
   }
@@ -20,13 +25,21 @@ class NavPanel extends Component {
             <p className="m-1">NavPanel</p>
 
             <div className="row">
-              <div className="col bg-secondary m-3" style={stylee.row}>Row 1, Col 1</div>
-              <div className="col bg-secondary m-3" style={stylee.row}>Row 1, Col 2</div>
+              <div className="col bg-secondary m-3" style={stylee.cell}>
+                <button className="btn" style={stylee.button}>Row 1, Col 1</button>
+              </div>
+              <div className="col bg-secondary m-3" style={stylee.cell}>
+                <button className="btn" style={stylee.button}>Row 1, Col 2</button>
+              </div>
             </div>
 
             <div className="row">
-              <div className="col bg-secondary m-3" style={stylee.row}>Row 2, Col 1</div>
-              <div className="col bg-secondary m-3" style={stylee.row}>Row 2, Col 2</div>
+              <div className="col bg-secondary m-3" style={stylee.cell}>
+                <button className="btn" style={stylee.button}>Row 2, Col 1</button>
+              </div>
+              <div className="col bg-secondary m-3" style={stylee.cell}>
+                <button className="btn" style={stylee.button}>Row 2, Col 2</button>
+              </div>
             </div>
         </div>
       );
