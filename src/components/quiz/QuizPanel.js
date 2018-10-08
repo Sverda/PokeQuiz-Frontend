b70ddbbfe5ec7b4eabf18fc401adf7451a38adce
Damian Ubowski <sverda300@gmail.com>
2018-10-08 21:30:18 +0200

    QuizPanel: Fetch data from API

--- src/components/quiz/QuizPanel.js
+++ src/components/quiz/QuizPanel.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 
 class QuizPanel extends Component {
-    render() {
-      return (
-        <div className="bg-secondary">
-            QuizPanel
-        </div>
-      );
-    }
+  constructor(){
+    super();
+    this.state = {
+      sprite: logo,
+      names: ["name_0", "name_1", "name_2", "name_3"]
+    };
   }
+
+  componentDidMount() {
+    fetch("http://localhost:50072/api/quiz")
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      let sprite = data.correctAnswer.image;
+      let names = data.fakeAnswers;
+      this.state.setState({
+        sprite: sprite,
+        names: names
+      });
+      console.log("state:", this.state.names, this .state.sprite);
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        QuizPanel
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="container row">
+          <button className="btn col-3">{this.state.names[0]}</button>
+          <button className="btn col-3">{this.state.names[1]}</button>
+          <button className="btn col-3">{this.state.names[2]}</button>
+          <button className="btn col-3">{this.state.names[3]}</button>
+        </div>
+      </div>
+    );
+  }
+}
   
   export default QuizPanel;
