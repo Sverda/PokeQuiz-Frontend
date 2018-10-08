a7c207b304440124a11c2180cf1a0b919327d84c
Damian Ubowski <damian.ubowski@softprime.pl>
2018-10-08 10:35:44 +0200

    Add padding to NavPanel

--- src/NavPanel.js
+++ src/NavPanel.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class NavPanel extends Component {
     render() {
       return (
-        <div className="bg-primary">
+        <div className="bg-primary m-4">
             NavPanel
         </div>
       );
