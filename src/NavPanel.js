26b899523b83eccc4ee02225e765b1885cf4eaf2
Damian Ubowski <damian.ubowski@softprime.pl>
2018-10-08 11:35:36 +0200

    NavPanel: Icons

--- src/NavPanel.js
+++ src/NavPanel.js
@@ -14,7 +14,8 @@ const stylee = {
 
   button: {
     width: 100,
-    height: 100
+    height: 100,
+    backgroundColor: "transparent"
   }
 };
 
@@ -26,19 +27,27 @@ class NavPanel extends Component {
 
             <div className="row">
               <div className="col bg-secondary m-3" style={stylee.cell}>
-                <button className="btn" style={stylee.button}>Row 1, Col 1</button>
+                <button className="btn btn-lg" style={stylee.button}>
+                  <i className="fa fa-play fa-2x" />
+                </button>
               </div>
               <div className="col bg-secondary m-3" style={stylee.cell}>
-                <button className="btn" style={stylee.button}>Row 1, Col 2</button>
+                <button className="btn" style={stylee.button}>
+                  <i className="fa fa-address-card fa-2x" />
+                </button>
               </div>
             </div>
 
             <div className="row">
               <div className="col bg-secondary m-3" style={stylee.cell}>
-                <button className="btn" style={stylee.button}>Row 2, Col 1</button>
+                <button className="btn" style={stylee.button}>
+                <i className="fa fa-arrow-left fa-2x"></i>
+                </button>
               </div>
               <div className="col bg-secondary m-3" style={stylee.cell}>
-                <button className="btn" style={stylee.button}>Row 2, Col 2</button>
+                <button className="btn" style={stylee.button}>
+                  <i className="fa fa-arrow-right fa-2x" />
+                </button>
               </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
