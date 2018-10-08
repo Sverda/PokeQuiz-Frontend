a436dc9be0c1097adf2f447b32664611cae8563f
Damian Ubowski <damian.ubowski@softprime.pl>
2018-10-08 11:01:58 +0200

    NavPanel: Setup grid for buttons

--- src/NavPanel.js
+++ src/NavPanel.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
+const stylee = {
+  navPanel: {
+    borderRadius: "15%",
+    width: "300px",
+    height: "300px"
+  },
+
+  row: {
+    width: 100,
+    height: 100
+  }
+};
+
 class NavPanel extends Component {
     render() {
       return (
-        <div className="bg-primary m-4">
-            NavPanel
+        <div className="container bg-primary m-4" style={stylee.navPanel}>
+            <p className="m-1">NavPanel</p>
+
+            <div className="row">
+              <div className="col bg-secondary m-3" style={stylee.row}>Row 1, Col 1</div>
+              <div className="col bg-secondary m-3" style={stylee.row}>Row 1, Col 2</div>
+            </div>
+
+            <div className="row">
+              <div className="col bg-secondary m-3" style={stylee.row}>Row 2, Col 1</div>
+              <div className="col bg-secondary m-3" style={stylee.row}>Row 2, Col 2</div>
+            </div>
         </div>
       );
     }
