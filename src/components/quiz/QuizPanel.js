81283d42abbd1c2ef794185d621ef5e65a9819a4
Damian Ubowski <sverda300@gmail.com>
2018-10-13 11:47:39 +0200

    QuizPanel: Keep user session

--- src/components/quiz/QuizPanel.js
+++ src/components/quiz/QuizPanel.js
@@ -11,27 +11,55 @@ const stylee = {
   }
 };
 
+const API = "http://localhost:50072/api/";
+const QUIZQUESTION = "quiz/question";
+const QUIZANSWER = "quiz/answer/{name}";
+
 class QuizPanel extends Component {
   constructor(){
     super();
+
     this.state = {
       sprite: "",
-      names: ["name_0", "name_1", "name_2", "name_3"]
+      names: ["name_0", "name_1", "name_2", "name_3"],
+      win: false
     };
+    this.cookie = "";
   }
 
   componentDidMount() {
-    fetch("http://localhost:50072/api/quiz")
+    fetch(API + QUIZQUESTION, {
+      method: "GET",
+      credentials: "include"
+    })
     .then(results => {
       return results.json();
     }).then(data => {
-      let sprite = data.correctAnswer.image;
-      let names = data.fakeAnswers;
-      this.setState({ sprite: sprite, names: names });
-      console.log("state:", this.state.names, this.state.sprite);
+      this.setState({ 
+        sprite: data.correctAnswer.image, 
+        names: data.fakeAnswers, 
+        win: false
+       });
     })
   }
 
+  sendAnswer(name, e) {
+    let url = API + QUIZANSWER.replace("{name}", name);
+    fetch(url, {
+      method: "GET",
+      credentials: "include"
+    })
+    .then(response => response.json())
+    .then(answer => {
+      console.log("answer: " + answer);
+      this.setState({
+        sprite: this.state.sprite,
+        names: this.state.names,
+        win: answer
+      });
+    });
+  }
+  
   render() {
     return (
       <div className="container">
@@ -39,10 +67,10 @@ class QuizPanel extends Component {
           <img src={"data:image/png;base64," + this.state.sprite} alt="sprite" className="img-fluid" style={stylee.img}/>
         </div>
         <div className="container row">
-          <button className="btn col-3">{this.state.names[0]}</button>
-          <button className="btn col-3">{this.state.names[1]}</button>
-          <button className="btn col-3">{this.state.names[2]}</button>
-          <button className="btn col-3">{this.state.names[3]}</button>
+          <button className="btn col-3" onClick={this.sendAnswer.bind(this, this.state.names[0])}>{this.state.names[0]}</button>
+          <button className="btn col-3" onClick={this.sendAnswer.bind(this, this.state.names[1])}>{this.state.names[1]}</button>
+          <button className="btn col-3" onClick={this.sendAnswer.bind(this, this.state.names[2])}>{this.state.names[2]}</button>
+          <button className="btn col-3" onClick={this.sendAnswer.bind(this, this.state.names[3])}>{this.state.names[3]}</button>
         </div>
       </div>
     );
